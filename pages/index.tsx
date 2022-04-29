9102eea53fb719bea18a79cb5d8f67a6e6150e11
Yuji Nakamura <ny5117@icloud.com>
2022-04-29 16:43:54 +0900

    importの順番を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from "next";
 import { useState } from "react";
+import type { NextPage } from "next";
 import Head from "next/head";
+
 import styled from "styled-components";
+
 import MainBoard from "../components/MainBoard";
 import SideMenu from "../components/SideMenu";
 
