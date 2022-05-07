12cdc9f1ee3fff4db8e8c76331a98272906ed61a
Yuji Nakamura <ny5117@icloud.com>
2022-05-07 11:01:12 +0900

    handlerのエンドポイントを追加

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,13 +3,20 @@ import { rest } from "msw";
 import { vocabularies } from "./data";
 
 export const handlers = [
-  // rest.post("/")
-
   rest.get("/api/vocabularies", (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(vocabularies));
   }),
 
   rest.post("/api/vocabulary", (_, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  rest.put("/api/vocabulary/:id", (req, res, ctx) => {
+    const { id } = req.params;
+    return res(ctx.status(200));
+  }),
+
+  rest.delete("/api/vocabulary/:id", (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
